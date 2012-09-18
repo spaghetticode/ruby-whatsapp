6b541fceda199e9add2edcffbff5f22286dbb7cb
Daniele Molteni <dani.m.mobile@gmail.com>
2012-09-18 08:05:18 +0200

    specs #token_map

--- spec/lib/binary_tree_node/writer_spec.rb
+++ spec/lib/binary_tree_node/writer_spec.rb
@@ -1,4 +1,9 @@
 require 'spec_helper'
 
 describe RubyWhatsapp::BinaryTreeNode::Writer do
+  describe '#token_map' do
+    it 'reduces the numbers of elements in the dictionary' do
+      subject.token_map.size.should == 240
+    end
+  end
 end
