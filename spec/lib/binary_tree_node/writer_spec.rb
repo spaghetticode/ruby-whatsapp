4aa694d54ee9316113fde7b9076f14cc8ee013b9
Daniele Molteni <dani.m.mobile@gmail.com>
2012-10-02 12:09:41 +0200

    clears the output before each test

--- spec/lib/binary_tree_node/writer_spec.rb
+++ spec/lib/binary_tree_node/writer_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe RubyWhatsapp::BinaryTreeNode::Writer do
+  before {subject.output.clear}
+
   def to_bytes string
     string.each_byte.inject([]) {|ary, b| ary << b} if string
   end
